fa1a95084fa69ce3e6dcd497a53a6836935843cc
NikkiBeeLili <arcanahereticae13@gmail.com>
2021-05-05 16:26:23 -0500

    setup

---
+++ Basic-User-Form/src/main/java/org/launchcode/Basic/User/Form/BasicUserFormApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Basic.User.Form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicUserFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicUserFormApplication.class, args);
+	}
+
+}
---
+++ Basic-User-Form/src/test/java/org/launchcode/Basic/User/Form/BasicUserFormApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Basic.User.Form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicUserFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
