8bbfb4aca3560e7b3c1aa3e6c6be362972becc22
NikkiBeeLili <arcanahereticae13@gmail.com>
2021-05-07 17:08:15 -0500

    Second Tutorial done

--- Basic-User-Form/src/main/java/main/java/org/launchcode/Basic/User/Form/Controllers/PlayListController.java
+++ Basic-User-Form/src/main/java/main/java/org/launchcode/Basic/User/Form/Controllers/PlayListController.java
@@ -1,11 +1,16 @@
 package main.java.org.launchcode.Basic.User.Form.Controllers;
 
+import main.java.org.launchcode.Basic.User.Form.Data.PlayListRepository;
 import main.java.org.launchcode.Basic.User.Form.Data.PlayListService;
 import main.java.org.launchcode.Basic.User.Form.Models.Playlists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -14,6 +19,9 @@ public class PlayListController {
 
     @Autowired
     private PlayListService playlistservice;
+
+    @Autowired
+    private PlayListRepository playlistrepository;
 
 
     @RequestMapping("/")
@@ -31,6 +39,22 @@ public class PlayListController {
         return "new_playlist";
     }
 
+    @RequestMapping(value="/save", method= RequestMethod.POST)
+    public String savePlaylist(@RequestParam Playlists playlists, Model model){
+        playlistservice.save(playlists);
+        return "/redirect";
+    }
+    @RequestMapping("/edit/ ${id}")
+    public ModelAndView showEditProductPage(@PathVariable(name="id") Long id){
+        ModelAndView mav = new ModelAndView("edit_playlist");
+        Playlists playlists = playlistservice.get(id);//Might have to modify this so that the interface has a getId method.
+        mav.addObject("Playlists", playlists);
+        return mav;
+    }
 
-
+    @RequestMapping("/delete/ ${id}")
+    public String deletePlaylist(@PathVariable(name="id") Long id){
+       playlistservice.delete(id);
+       return "redirect:/";
+    }
 }
